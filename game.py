8a9e3ecbc84962d0617a4fb40cc782ce138cf60c
Breno Cardoso <dev.brenosspc@gmail.com>
2025-11-01 08:42:58 -0300

    Update game.py

--- game.py
+++ game.py
@@ -40,7 +40,12 @@ class Heroi(Personagem):
     
     def exibir_detalhes(self):
         return f"{super().exibir_detalhes()}\nHabilidade: {self.get_habilidade()}\n"
-    
+
+    def ataque_especial(self, alvo):
+        dano = self.get_nivel() * 5
+        alvo.receber_ataque(dano)
+        print(f"{self.get_nome()} usou a habilidade especial {self.get_habilidade()} em {alvo.get_nome()} e causou {dano} de dano")
+
 class Inimigo(Personagem):
     def __init__(self, nome, vida, nivel, tipo):
         super().__init__(nome, vida, nivel)
@@ -67,10 +72,15 @@ class Jogo:
 
             input("Pressione Enter para atacar...")
             escolha = input ("Escolha (1 - Ataque normal, 2 - Ataque Especial): ")
-            if escolha == "1":
+            if escolha == '1':
                 self.heroi.atacar(self.inimigo)
+            elif escolha == '2':
+                self.heroi.ataque_especial(self.inimigo)
             else:
                 print("Escolha invalida. Escolha novamente")
+            
+            if self.inimigo.get_vida() > 0:
+                self.inimigo.atacar(self.heroi)
         if self.heroi.get_vida() > 0:
             print("\nParabens, voce venceu a batalha")
         else:
@@ -79,8 +89,3 @@ class Jogo:
 # criar instancia do jogo e iniciar batalha
 jogo = Jogo()
 jogo.iniciar_batalha()
-
-heroi = Heroi(nome="Ethan", vida=100, nivel=5, habilidade="Super Forca")
-print(heroi.exibir_detalhes())
-inimigo = Inimigo(nome="Morcego", vida=50, nivel=3, tipo="Voador")
-print(inimigo.exibir_detalhes())
